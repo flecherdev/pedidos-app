12d16cf087a066f6de8b0e0c8386754ada60629e
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2021-08-03 21:16:35 -0300

    update to angular 12

--- src/app/pedido/components/pedido-form/pedido-form.component.spec.ts
+++ src/app/pedido/components/pedido-form/pedido-form.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PedidoFormComponent } from './pedido-form.component';
 
@@ -6,7 +6,7 @@ describe('PedidoFormComponent', () => {
   let component: PedidoFormComponent;
   let fixture: ComponentFixture<PedidoFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PedidoFormComponent ]
     })
